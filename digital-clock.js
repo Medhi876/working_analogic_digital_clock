8bb134b6fd14b64d2a16489270218c733f63a2da
Mlafran-design <medhi876@gmail.com>
2022-06-04 13:43:43 +0200

    solving a bug in the js of the clock

--- digital-clock.js
+++ digital-clock.js
@@ -9,7 +9,32 @@ setInterval(() => {
     let mm = day.getMinutes() * 6;
     let ss = day.getSeconds() * 6;
 
-    // hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
-    // mn.style.transform = `rotateZ(${mm}deg)`;
-    // sc.style.transform = `rotateZ(${ss}deg)`;
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+
+    //Digital-clock
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    //ampm
+    let am = h >= 12 ? "PM" : "AM"; //teste iiiiiiiiiiiiiiiiiiiiiiiiiii
+    //convert 24h clock to 12h clock
+    // if (h > 12) {
+    //     h = h - 12;
+    // }
+    //add zero before single digit number
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
 })
